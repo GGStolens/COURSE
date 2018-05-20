a44f043442654cc04bdb6e9fba1995e3b7404066
GGStolens <anatoliy.kichev@gmail.com>
2018-05-20 13:36:06 +0300

    Protoype_0.1

---
+++ interface.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "polynom.h"
+#include "interface.h"
+
+void menu() {
+	using namespace std;
+	cout << "/n";
+	cout << "1. " << "4. /n";
+	cout << "2. " << "5. /n";
+	cout << "3. " << "0. /n";
+}
---
+++ interface.h
@@ -0,0 +1,9 @@
+#ifndef INTERFACE_H_
+#define INTERFACE_H_
+
+#include "polynom.h"
+
+void menu();
+void new_polynom(Polynom & p, )
+
+#endif
